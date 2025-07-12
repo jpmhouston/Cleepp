1307651436e9afeaa742da0b4cf33b533b0eff0e
Pierre Houston <jpmhouston+github@gmail.com>
2025-07-11 18:55:59 -0700

    Correct links in aboutbox & intro

--- Cleepp/Cleepp+Links.swift
+++ Cleepp/Cleepp+Links.swift
@@ -12,15 +12,15 @@ extension Cleepp {
   
   static var githubURL = "https://github.com/jpmhouston/Cleepp"
   static var homepageURL = "https://batchclipboard.bananameter.lol"
-  static var homepageBonusDocsURL = "https://github.com/jpmhouston/Cleepp/wiki/Bonus-Features-for-the-Mac-App-Store-Version"
+  static var homepageBonusDocsURL = "https://batchclipboard.bananameter.lol/App-Store-Version-Differences"
   static var donationURL = "https://www.buymeacoffee.com/bananameterlabs"
-  static var macAppStoreURL = "https://github.com/jpmhouston/Cleepp/wiki/Available-in-the-Mac-App-Store-Soon"
+  static var macAppStoreURL = "https://apps.apple.com/app/batch-clipboard/id6695729238"
   static var appStoreUserAgreementURL = "https://www.apple.com/legal/internet-services/itunes/dev/stdeula/"
-  static var privacyPolicyURL = "https://github.com/jpmhouston/Cleepp/wiki/Privacy-Policy"
+  static var privacyPolicyURL = "https://batchclipboard.bananameter.lol/Privacy-Policy"
   static var maccyURL = "https://maccy.app"
-  static var supportEmailAddress = "batchclip.support@bananameter.lol"
+  static var supportEmailAddress = "batchclipboard.support@bananameter.lol"
   static var supportEmailURL = "mailto:" + Cleepp.supportEmailAddress
-  static var localizeVolunteerEmailAddress = "batchclip.l10nhelp@bananameter.lol"
+  static var localizeVolunteerEmailAddress = "batchclipboard.l10nhelp@bananameter.lol"
   static var localizeVolunteerEmailURL = "mailto:" + Cleepp.localizeVolunteerEmailAddress
   static var showIntroInAppURL = "batchclipapp:intro"
   static var showIntroPermissionPageInAppURL = "batchclipapp:intro/permissions"
